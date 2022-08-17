b22c963e392b8cbd7b687f57b13633b734209e76
y-temp4 <y.temp4@gmail.com>
2022-08-17 09:49:15 +0900

    Add sample top page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryLighter: '#57b9f2',
+        primary: '#1c7ed6',
+        primaryDarker: '#315ce4',
+        text: '#0a1c3e',
+      },
+    },
   },
   plugins: [],
 }
