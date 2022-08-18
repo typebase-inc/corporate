3d3bde3190513e1bac38c28cf1d98ca713bd04b1
y-temp4 <y.temp4@gmail.com>
2022-08-18 14:14:20 +0900

    Add twitter meta info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,6 +41,8 @@ const Index: NextPage = () => {
         <meta property="og:description" content={description} />
         <meta property="og:site_name" content={title} />
         <meta property="og:image" content={thumbnail} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@typebase_inc" />
         <meta name="viewport" content="width=device-width" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
